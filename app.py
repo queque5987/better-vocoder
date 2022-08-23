98bc330d1b0c5f1ef7ad8f8a885a759c4efaaa65
queque5987 <pyw5987@gmail.com>
2022-08-23 15:04:06 +0900

    20220823 1504

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import numpy as np
+import json
+import requests
 
+from mysql_connect import mysql_connect
 import rtvc_main
 
 app = FastAPI()
 
 class UserInput(BaseModel):
     spec: list
+    sr: int
 
 @app.get('/')
 def index():
@@ -19,8 +23,31 @@ def index():
 def inference(userinput: UserInput):
     userinput = userinput.dict()
     spec = np.array(userinput["spec"])
+    sr = userinput["sr"]
     print("spec uploaded")
     wav = rtvc_main.inference(spec)
+
+    wav = np.pad(wav, (0, sr), mode="constant")
+    wav = list(wav)
+    wav_json = json.dumps({
+        "wav": wav,
+        "sr": sr
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    print("requesting preprocessing to encoder . . .")
+    response = requests.request("GET", "https://better-encoder.herokuapp.com/preprocess/", headers=headers, data=wav_json)
+    wav = response.json()
     print(wav)
-    wav = jsonable_encoder(wav.tolist())
-    return JSONResponse(wav)
+    print("connecting to mysql server")
+    msq = mysql_connect()
+    print("sending to mysql server")
+    msq.send_wav(wav)
+    print("wav sent to mysql server")
+
+    # return "done"
+    # wav = jsonable_encoder("done")
+    return JSONResponse(jsonable_encoder("done"))
+    # wav = jsonable_encoder(wav.tolist())
+    # return JSONResponse(wav)
